1872ca91caa3c58380ece37fd922dc75770e8ea9
Kévin Bonnot <bonnot.kevin@gmail.com>
2023-07-22 05:11:11 +0200

    Cors policy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import 'dotenv/config';
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send({coucou: 'coucou'});
