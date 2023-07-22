da62bafafae161af1ad3ca478bbcfdf82d9bc3d7
Kévin Bonnot <bonnot.kevin@gmail.com>
2023-07-22 02:00:56 +0200

    Project init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import 'dotenv/config';
+import routes from "./routes";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send({coucou: 'coucou'})
+});
+
+app.use('/api', routes);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
